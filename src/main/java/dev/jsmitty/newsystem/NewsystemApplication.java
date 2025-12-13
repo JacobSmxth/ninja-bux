eb8a5b34231fd72d1f5d2539b8c8c08cb7fd6df5
Jacob <jacob.d.smith@live.com>
2025-12-13 15:33:45 -0500

    chore: initial Spring Boot project (generated by springseed)

---
+++ src/main/java/dev/jsmitty/newsystem/NewsystemApplication.java
@@ -0,0 +1,13 @@
+package dev.jsmitty.newsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jsmitty/newsystem/NewsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jsmitty.newsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
